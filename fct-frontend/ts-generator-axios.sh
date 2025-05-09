954b5fbb81e3e8c1fa22fd9b70c0d60413631514
Jose Vasquez <joseevasquez04@gmail.com>
2025-05-09 15:50:33 +0200

    updated dockerfiles and docker-compose

--- fct-frontend/ts-generator-axios.sh
+++ fct-frontend/ts-generator-axios.sh
@@ -1,6 +1,6 @@
 rm -rf ./src/api
 
-SPEC_URL="${OPENAPI_URL:-../fct-backend/src/main/resources/openapi/openapi.yaml}"
+SPEC_URL="${OPENAPI_URL:-./api-spec/openapi.yaml}"
 echo "Generating API client from $SPEC_URL"
 
 openapi-generator-cli generate \
